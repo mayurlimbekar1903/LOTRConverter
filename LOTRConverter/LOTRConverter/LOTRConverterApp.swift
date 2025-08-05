0b45d4cc6d0e5cd8f206d3db0f3db815cc994950
mayur limbekar <mayurlimbekar7@gmail.com>
2025-08-05 18:41:09 +0530

    initial study setup

---
+++ LOTRConverter/LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Mayur Limbekar on 05/08/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
